6413a1ed2a0c49281103979041f5c3eaf2f474ee
pegedi <peter.egedi@gmail.com>
2019-11-10 22:40:43 +0000

    buttons added

--- 01-local-state/src/App.js
+++ 01-local-state/src/App.js
@@ -5,12 +5,24 @@ class Counter extends Component {
         this.state = {
             counter: 0,
         };
+        this.onIncrement = this.onIncrement.bind(this);
+        this.onDecrement = this.onDecrement.bind(this);
+    }
+
+    onIncrement() {
+        this.setState({counter: this.state.counter + 1});
+    }
+
+    onDecrement() {
+        this.setState({counter: this.state.counter - 1});
     }
 
     render () {
         return (
             <div>
                 <p>{this.state.counter}</p>
+                <button type="button" onClick = {this.onIncrement}>Increment</button>
+                <button type="button" onClick = {this.onDecrement}>Decrement</button>
             </div>
         );
     }
