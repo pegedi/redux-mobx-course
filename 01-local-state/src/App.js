1823bdb5b105acc6a4da363f9e1fd0cfbdc20bc8
pegedi <peter.egedi@gmail.com>
2019-11-10 23:31:35 +0000

    Stateless function added - Container/Presenter modell

--- 01-local-state/src/App.js
+++ 01-local-state/src/App.js
@@ -1,4 +1,21 @@
 import React, {Component} from 'react';
+
+export class App extends Component {
+    constructor (props) {
+        super(props);
+    }
+    render () {
+        return (
+            <>
+                <Counter />
+                <hr/>
+                <CounterContainer />
+            </>
+
+        );
+    }
+}
+
 class Counter extends Component {
     constructor (props) {
         super(props);
@@ -26,5 +43,40 @@ class Counter extends Component {
             </div>
         );
     }
+}
+
+class CounterContainer extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+        this.onIncrement = this.onIncrement.bind(this);
+        this.onDecrement = this.onDecrement.bind(this);
+    }
+    onIncrement () {
+        this.setState ({counter: this.state.counter + 1});
+    }
+    onDecrement () {
+        this.setState ({counter: this.state.counter - 1});
+    }
+    render () {
+        return (
+            <CounterPresenter
+                counter = {this.state.counter}
+                onIncrement = {this.onIncrement}
+                onDecrement = {this.onDecrement}
+            />
+        );
+    }
+}
+function CounterPresenter (props) {
+    return (
+        <div>
+          <p>{props.counter}</p>
+          <button type="button" onClick = {props.onIncrement}>Increment</button>
+          <button type="button" onClick = {props.onDecrement}>Decrement</button>
+        </div>
+    );
 }
 export default Counter;
