c19d1ca9dc0f0c9ce913e0aa5b882aa5d9476f9d
pegedi <peter.egedi@gmail.com>
2019-12-26 23:15:32 +0000

    Combined reducers added

--- 02-redux/src/02-redux-basic.js
+++ 02-redux/src/02-redux-basic.js
@@ -1,9 +1,13 @@
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 
 const TODO_ADD = 'TODO_ADD';
 const TODO_TOGGLE = 'TODO_TOGGLE';
 const FILTER_SET = 'FILTER_SET';
-const store = createStore(rootReducer);
+
+const rootReducer = combineReducers({
+    todoState: todoReducer,
+    filterState: filterReducer,
+});
 
 function todoReducer (state=[], action){
     switch (action.type) {
@@ -19,28 +23,75 @@ function todoReducer (state=[], action){
 //nested state Object!!!
 function applyAddTodo(state, action){
     const todo = Object.assign({}, action.todo, {completed: false});
-    const todos = state.todos.concat(todo);
-    return Object.assign({}, state, {todos });
+    return state.concat(todo);
 }
 function applyToggleTodo(state, action) {
-    const todos = state.todos.map(todo => todo.id === action.todo.id ?
-                                            Object.assign({}, todo, {completed: !todo.completed}) :
-                                            todo);
-    return Object.assign({}, state, {todos});
+    const todos = state.map(todo => todo.id === action.todo.id ?
+            Object.assign({}, todo, {completed: !todo.completed}) :
+            todo);
+    return todos;
 }
 
-function filterReducer(state='SHAW ALL', action) {
+function filterReducer(state='SHOW ALL', action) {
     switch (action.type) {
-        case FILTER_SET: {
-            applySetFilter(state, action);
+        case TODO_TOGGLE: {
+            return 'Helloka - both reducers pick the action.type';
         }
+        case FILTER_SET: {
+            return applySetFilter(state, action);
+        }
+
         default: {
             return state;
         }
     }
 }
+/**
+ * # Heading Level 1
+ * ## Header 2.
+ * Function to set _filter_ state
+ *
+ * or ___filter___ state
+ *
+ *
+ */
+function applySetFilter(state, action){
+    return action.filter;
+}
 
+function doSetFilter(filter){
+    return {
+        type: FILTER_SET,
+        filter,
+    }
+}
+function doAddTodo(id, name){
+    return {
+        type: TODO_ADD,
+        todo: {id, name} ,
+    }
+}
+function doToggleTodo(id){
+    return {
+        type: TODO_TOGGLE,
+        todo: {id},
+    }
+}
 export function reduxBasic02(){
 
-    const store = createStore(reducer, []);
+    const store = createStore(rootReducer);
+    console.log('initial state:');
+    console.log(store.getState());
+    const unsubscribe = store.subscribe(() => {
+        console.log('Store updated, current state:');
+        console.log(store.getState());
+    })
+
+    store.dispatch(doAddTodo('0', 'learn redux'));
+    store.dispatch(doAddTodo('1', 'learn mobx'));
+    store.dispatch(doToggleTodo('0'));
+    store.dispatch(doSetFilter('COMPLETED'));
+
+    unsubscribe();
+
 }
--- 02-redux/src/App.js
+++ 02-redux/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {proba} from './01-redux-basic';
+import {reduxBasic02} from './02-redux-basic';
 
 function App() {
   return (
-    <h1>
-      
-          Learn React
-           {proba()}
-     
-    </h1>
+    <>
+        <h1>
+
+            Learn React
+            {proba()}
+
+        </h1>
+        <hr/>
+        <h1>
+            Learn Redux
+            {reduxBasic02()}
+        </h1>
+    </>
   );
 }
 
